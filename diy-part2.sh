37e9453f30b80351038f8ce42ecc2a0c252fac4b
yanfurong <59911089+yanfurong@users.noreply.github.com>
2020-12-05 16:09:38 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,8 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.6.222/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/HP GEN8/g' package/base-files/files/bin/config_generate
+git clone https://github.com/frainzy1477/luci-app-clash.git package/luci-app-clash
+git clone https://github.com/vernesong/OpenClash.git -b master package/openclash 
+mv package/openclash/luci-app-openclash  package/luci-app-openclash  
